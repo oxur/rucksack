dd769926eae72b82c6df37dbb95f366cb572bb2a
Duncan McGreggor <oubiwann@gmail.com>
2022-12-29 16:58:47 -0600

    Added initial password gen.

--- src/cli/command.rs
+++ src/cli/command.rs
@@ -2,6 +2,25 @@ use anyhow::{Result};
 use clap::ArgMatches;
 
 // pub fn generate(matches: &ArgMatches, config: &config::Config) -> Result<()> {
-pub fn generate(_matches: &ArgMatches) -> Result<()> {
+pub fn generate(matches: &ArgMatches) -> Result<()> {
+    match matches.get_one::<String>("type").map(|s| s.as_str()) {
+        Some("uuid") => generate_pwd_uuid(),
+        Some("default") => generate_pwd_uuid(),
+        Some(_) => todo!(),
+        None => todo!(),
+    }
+}
+
+fn generate_pwd_uuid() -> Result<()> {
+    let pwd = uuid::Uuid::new_v4().to_string();
+    display(&pwd)
+}
+
+pub fn version(version: &str) -> Result<()> {
+    display(version)
+}
+
+fn display(text: &str) -> Result<()> {
+    println!("{}", text);
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,42 @@ use std::{io};
 // use anyhow::{anyhow, Context, Result};
 use anyhow::{Context, Result};
 // use clap::builder::{EnumValueParser, PossibleValuesParser, ValueParser};
-use clap::{Arg, ArgMatches, Command};
+use clap::builder::{EnumValueParser};
+use clap::{Arg, ArgAction, ArgMatches, Command};
 
 mod cli;
 
-const TOOL_NAME: &str = "rucksack";
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const DESC: &str = env!("CARGO_PKG_DESCRIPTION");
 
 fn cli() -> Command {
-    Command::new(TOOL_NAME)
-    .about("A terminal-based password manager, importer (Firefox Sync), and generator")
-    .subcommand_required(true)
+    Command::new(NAME)
+    .about(DESC)
     .arg_required_else_help(true)
     .allow_external_subcommands(true)
+    .arg(
+        Arg::new("completions")
+            .help("emit shell tab completions")
+            .long("completions")
+            .value_name("SHELL")
+            .value_parser(EnumValueParser::<clap_complete::Shell>::new()),
+    )
+    .arg(
+        Arg::new("version")
+            .help("Print version information")
+            .short('v')
+            .long("version")
+            .action(ArgAction::SetTrue)
+    )
     .subcommand(
         Command::new("gen")
             .about("generate a secret")
             .arg(
                 Arg::new("type")
                     .help("the type of generator to use")
-                    .index(1)
+                    .short('t')
+                    .long("type")
                     .default_value("default"),
             ),
     )
@@ -33,8 +50,8 @@ fn cli() -> Command {
 fn run(matches: &ArgMatches) -> Result<()> {
     match matches.subcommand() {
         Some(("gen", matches)) => cli::command::generate(matches)?,
-        Some((&_, _)) => todo!(),
-        None => todo!(),
+            Some((&_, _)) => todo!(),
+            None => todo!(),
     }
     Ok(())
 }
@@ -45,8 +62,12 @@ fn main() -> Result<()> {
 
     // Shell completion generation is completely independent, so perform it before
     // any config or subcommand operations.
-    if let Some(shell) = matches.get_one::<clap_complete::Shell>("completions") {
-        clap_complete::generate(*shell, &mut rucksack, env!("CARGO_PKG_NAME"), &mut io::stdout());
+    if let Some(is_version) = matches.get_one::<bool>("version") {
+        if *is_version {
+            return cli::command::version(VERSION);
+        }
+    } else if let Some(shell) = matches.get_one::<clap_complete::Shell>("completions") {
+        clap_complete::generate(*shell, &mut rucksack, NAME, &mut io::stdout());
         return Ok(());
     }
 
