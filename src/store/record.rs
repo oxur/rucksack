0dcd7b34bd7f6c7db18c9ae21666978c667879f4
Duncan McGreggor <oubiwann@gmail.com>
2022-12-30 15:47:07 -0600

    Latest bincode and use secrecy.

--- src/store/record.rs
+++ src/store/record.rs
@@ -1,14 +1,14 @@
+use bincode::{config, Decode, Encode};
+use secrecy::Zeroize;
+use serde::{Deserialize, Serialize};
 
-// use serde::{Serialize, Deserialize};
-use serde_derive::{Serialize, Deserialize};
-
-#[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Debug, Default, Serialize, Deserialize, Eq, PartialEq, Encode, Decode)]
 pub enum Kind {
     #[default]
-    Password
+    Password,
 }
 
-#[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq, Encode, Decode)]
 pub struct EncryptedRecord {
     pub key: String,
     pub kind: Kind,
@@ -17,14 +17,31 @@ pub struct EncryptedRecord {
     pub updated: String,
 }
 
-#[derive(Clone, Debug, Default, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Serialize, Deserialize, Eq, PartialEq, Encode, Decode)]
 pub struct PasswordValue {
     pub user: String,
     pub password: String,
-
 }
 
-#[derive(Clone, Debug, Default, PartialEq)]
+impl Zeroize for PasswordValue {
+    fn zeroize(&mut self) {
+        self.password.zeroize();
+    }
+}
+
+impl std::fmt::Display for PasswordValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "PasswordValue{{user: {}, password: *****}}", self.user)
+    }
+}
+
+impl std::fmt::Debug for PasswordValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "PasswordValue{{user: {}, password: *****}}", self.user)
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Encode, Decode)]
 pub struct DecryptedRecord {
     pub key: String,
     pub kind: Kind,
@@ -35,10 +52,10 @@ pub struct DecryptedRecord {
 
 impl DecryptedRecord {
     pub fn encrypt(&self) -> EncryptedRecord {
-        EncryptedRecord{
+        EncryptedRecord {
             key: self.key.clone(),
             kind: self.kind.clone(),
-            value: bincode::serialize(&self.value).unwrap(),
+            value: bincode::encode_to_vec(&self.value, config::standard()).unwrap(),
             created: self.created.clone(),
             updated: self.updated.clone(),
         }
@@ -47,14 +64,16 @@ impl DecryptedRecord {
 
 impl EncryptedRecord {
     pub fn decrypt(&self) -> DecryptedRecord {
-        DecryptedRecord{
+        let (decoded, _len) =
+            bincode::decode_from_slice(&self.value[..], config::standard()).unwrap();
+        DecryptedRecord {
             key: self.key.clone(),
             kind: self.kind.clone(),
-            value: bincode::deserialize(&self.value[..]).unwrap(),
+            value: decoded,
             created: self.created.clone(),
             updated: self.updated.clone(),
         }
-}
+    }
 }
 
 #[cfg(test)]
@@ -64,20 +83,31 @@ mod tests {
     #[test]
     fn password_records() {
         let now = chrono::offset::Local::now().to_rfc3339();
-        let dpr = DecryptedRecord{
+        let dpr = DecryptedRecord {
             key: "a site".to_string(),
             kind: Kind::Password,
-            value: PasswordValue{
+            value: PasswordValue {
                 user: "alice@site.com".to_string(),
                 password: "4 s3kr1t".to_string(),
             },
             created: now.clone(),
-            updated: now.clone(),
+            updated: now,
         };
+        assert_eq!(
+            format!("{}", dpr.value),
+            "PasswordValue{user: alice@site.com, password: *****}"
+        );
+        assert_eq!(
+            format!("{:?}", dpr.value),
+            "PasswordValue{user: alice@site.com, password: *****}"
+        );
         let epr = dpr.encrypt();
         assert_eq!(
             epr.value,
-            [14, 0, 0, 0, 0, 0, 0, 0, 97, 108, 105, 99, 101, 64, 115, 105, 116, 101, 46, 99, 111, 109, 8, 0, 0, 0, 0, 0, 0, 0, 52, 32, 115, 51, 107, 114, 49, 116]
+            [
+                14, 97, 108, 105, 99, 101, 64, 115, 105, 116, 101, 46, 99, 111, 109, 8, 52, 32,
+                115, 51, 107, 114, 49, 116
+            ]
         );
         let re_dpr = epr.decrypt();
         assert_eq!(re_dpr.value.password, "4 s3kr1t");
