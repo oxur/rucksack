ffc029e5588f79e6d299cf4481acb0ba9b1ebe84
Duncan McGreggor <oubiwann@gmail.com>
2023-01-03 17:03:28 -0600

    Added support for sorting, etc.

--- src/cli/command/list.rs
+++ src/cli/command/list.rs
@@ -4,33 +4,74 @@ use passwords::{analyzer, scorer};
 
 use super::util;
 
+#[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
+struct ListResult {
+    url: String,
+    user: String,
+    pwd: String,
+    score: i64,
+}
+
+fn new_result(user: String, url: String) -> ListResult {
+    ListResult {
+        user,
+        url,
+
+        ..Default::default()
+    }
+}
+
 pub fn all(matches: &ArgMatches) -> Result<()> {
     let decrypt = matches.get_one::<bool>("decrypt");
+    let reveal = matches.get_one::<bool>("reveal");
+    let sort_by = matches.get_one::<String>("sort-by").map(|s| s.as_str());
     let db = util::setup_db(matches)?;
-    match decrypt {
-        Some(true) => decrypted_header(),
-        Some(false) => encrypted_header(),
-        None => encrypted_header(),
-    }
+    let mut results: Vec<ListResult> = Vec::new();
     for i in db.iter() {
         let record = i.value().decrypt(db.store_pwd(), db.salt())?;
+        let mut result = new_result(record.user(), record.metadata().url);
+        let hidden = "*".repeat(10).to_string();
         match decrypt {
             Some(true) => {
-                let analyzed = analyzer::analyze(record.creds.password.clone());
+                let analyzed = analyzer::analyze(record.password());
                 let score = scorer::score(&analyzed);
-                println!(
-                    "{: <40} | {: <30} | {: <20} | {:.2}",
-                    record.metadata().url,
-                    record.creds.user,
-                    record.creds.password,
-                    score
-                )
+                let pwd = match reveal {
+                    Some(true) => record.password(),
+                    Some(false) => hidden,
+                    None => unreachable!(),
+                };
+                result.pwd = pwd;
+                result.score = score.trunc() as i64;
             }
-            Some(false) => println!("{: <40} | {}", record.metadata().url, record.creds.user),
-            None => println!("{:?}", record),
+            Some(false) => result.pwd = hidden,
+            None => unreachable!(),
+        }
+        results.push(result)
+    }
+    match sort_by {
+        Some("score") => results.sort_by(|a, b| b.score.cmp(&a.score)),
+        Some("url") => results.sort(),
+        Some("user") => results.sort_by(|a, b| a.user.cmp(&b.user)),
+        Some(&_) => (),
+        None => (),
+    };
+    display_results(results, decrypt);
+    Ok(())
+}
+
+fn display_results(sorted: Vec<ListResult>, decrypted: Option<&bool>) {
+    match decrypted {
+        Some(true) => decrypted_header(),
+        Some(false) => encrypted_header(),
+        None => unreachable!(),
+    }
+    for r in sorted {
+        match decrypted {
+            Some(true) => decrypted_result(r),
+            Some(false) => encrypted_result(r),
+            None => unreachable!(),
         }
     }
-    Ok(())
 }
 
 const URL_HEADER: &str = "URL";
@@ -55,4 +96,15 @@ fn decrypted_header() {
 fn encrypted_header() {
     println!("{: <40} | {: <30}", URL_HEADER, USER_HEADER);
     println!("{: <40}-+-{}", "-".repeat(40), "-".repeat(30))
+}
+
+fn decrypted_result(r: ListResult) {
+    println!(
+        "{: <40} | {: <30} | {: <20} | {:.2}",
+        r.url, r.user, r.pwd, r.score
+    )
+}
+
+fn encrypted_result(r: ListResult) {
+    println!("{: <40} | {}", r.url, r.user)
 }
