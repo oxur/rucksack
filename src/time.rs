21e0b40bff12404882d96bb38158e78633c92a66
Duncan McGreggor <oubiwann@gmail.com>
2023-01-05 00:56:38 -0600

    Added mising file.

---
+++ src/time.rs
@@ -0,0 +1,27 @@
+use chrono::offset::Local;
+use chrono::{DateTime, TimeZone, Utc};
+
+pub fn simple_timestamp() -> String {
+    chrono::offset::Local::now()
+        .format("%Y%m%d-%H%M%S")
+        .to_string()
+}
+
+pub fn now() -> String {
+    Local::now().to_rfc3339()
+}
+
+pub fn epoch_to_string(e: i64) -> String {
+    Utc.timestamp_millis_opt(e).unwrap().to_rfc3339()
+}
+
+pub fn string_to_epoch(stamp: String) -> i64 {
+    match DateTime::parse_from_rfc3339(&stamp) {
+        Ok(dt) => dt.timestamp_millis(),
+        Err(e) => {
+            // TODO: change to debug logging
+            println!("{:?}", e);
+            Local::now().timestamp_millis()
+        }
+    }
+}
