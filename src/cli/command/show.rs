34bd83179da2a56b543caaf8d4f16934365764c8
Duncan McGreggor <oubiwann@gmail.com>
2023-01-18 00:32:50 -0600

    Implemented two 'show' commands.

--- src/cli/command/show.rs
+++ src/cli/command/show.rs
@@ -1,17 +1,23 @@
+use std::str;
+
 use anyhow::Result;
 use clap::ArgMatches;
 
 use crate::app::App;
+use crate::util;
 
-pub fn config_file(matches: &ArgMatches, app: &App) -> Result<()> {
-    log::info!("matches: {:?}; app: {:?}", matches, app);
-    log::warn!("not yet implemented");
+pub fn config_file(_matches: &ArgMatches, app: &App) -> Result<()> {
+    println!("\n{}\n", app.config_file());
     Ok(())
 }
 
-pub fn config(matches: &ArgMatches, app: &App) -> Result<()> {
-    log::info!("matches: {:?}; app: {:?}", matches, app);
-    log::warn!("not yet implemented");
+pub fn config(_matches: &ArgMatches, app: &App) -> Result<()> {
+    match util::read_file(app.config_file()) {
+        Ok(bytes) => {
+            println!("\n{}\n", str::from_utf8(bytes.as_ref()).unwrap());
+        }
+        Err(e) => panic!("{}", e),
+    }
     Ok(())
 }
 
