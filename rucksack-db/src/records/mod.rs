eeec2ef0b93615d8cd5d9292644e04db28fc277c
Duncan McGreggor <oubiwann@gmail.com>
2023-02-12 21:19:56 -0600

    Looks like v080's not ready either.

--- rucksack-db/src/records/mod.rs
+++ rucksack-db/src/records/mod.rs
@@ -9,7 +9,7 @@ pub mod v080;
 pub mod v090;
 
 // The aliases here are taken from the most recent versions:
-pub use v080::{
+pub use v070::{
     decode_hashmap, default_metadata, default_secrets, key, new_tag, new_tags,
     secrets_from_user_pass, types, DecryptedRecord, EncryptedRecord, HashMap, Kind, Metadata,
     Secrets, Status, Tag, ANY_CATEGORY, DEFAULT_CATEGORY, VERSION,
