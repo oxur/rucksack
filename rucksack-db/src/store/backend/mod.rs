3bed8bc81f3f8fe45319598007f06e79e4a0d7ed
Duncan McGreggor <oubiwann@gmail.com>
2023-12-20 00:03:21 -0600

    We're not going to be using persy as a backend.

--- rucksack-db/src/store/backend/mod.rs
+++ rucksack-db/src/store/backend/mod.rs
@@ -8,7 +8,5 @@ pub mod redb;
 
 #[cfg(feature = "filesystem")]
 pub use crate::store::backend::filesystem::FileSystemBackend;
-#[cfg(feature = "persy")]
-pub use crate::store::backend::persy::PersyBackend;
 #[cfg(feature = "redb")]
 pub use crate::store::backend::redb::ReDBBackend;
